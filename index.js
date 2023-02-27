fd4e6e8bbb0dd77650c53619bd8057722c8040ee
Gini Stanzi <a.stanzione14@gmail.com>
2023-02-27 21:19:26 +0000

    fixed HTML rendering

--- index.js
+++ index.js
@@ -17,146 +17,115 @@ let team = [];
 
 
 questionPrompt()
-async function questionPrompt(){
-const data = await inquirer 
-.prompt([
-    {
-        name: 'employee_type',
-        type: 'list',
-        message: "What type of employee would you like to create a profile for?",
-        choices: ['Manager', 'Engineer', 'Intern'],
-    },
-]);
+async function questionPrompt() {
+    const data = await inquirer
+        .prompt([
+            {
+                name: 'employee_type',
+                type: 'list',
+                message: "What type of employee would you like to create a profile for?",
+                choices: ['Manager', 'Engineer', 'Intern', 'Done Adding.'],
+            },
+        ]);
 
-if (data.employee_type == 'Manager'){
-    const managerData = await inquirer
-    .prompt([
-        {
-            name: 'name',
-            message: "What is the employee's full name?",
-            type: 'input',
-        },
-        {
-            name: 'id',
-            type: 'input',
-            message: "What is the employee's ID number?",
-        },
-        {
-            name: 'email',
-            type: 'input',
-            message: "What is the employee's email address?",
-        },
-        {
-            name: 'office_number',
-            message: "What is the manager's office number?",
-            type: 'input',
-        },
-    ]
-    )
-    const thisManager = new Manager(managerData.name, managerData.id, managerData.email, managerData.office_number)
-    team.push(thisManager)
-    startProgram()
-    async function startProgram(){
-    let htmlDoc = render(team)
-    await fs.writeFile(outputPath, htmlDoc)}
-    console.log(team)
+    if (data.employee_type == 'Manager') {
+        const managerData = await inquirer
+            .prompt([
+                {
+                    name: 'name',
+                    message: "What is the employee's full name?",
+                    type: 'input',
+                },
+                {
+                    name: 'id',
+                    type: 'input',
+                    message: "What is the employee's ID number?",
+                },
+                {
+                    name: 'email',
+                    type: 'input',
+                    message: "What is the employee's email address?",
+                },
+                {
+                    name: 'office_number',
+                    message: "What is the manager's office number?",
+                    type: 'input',
+                },
+            ]
+            )
+        const thisManager = new Manager(managerData.name, managerData.id, managerData.email, managerData.office_number)
+        team.push(thisManager)
+        questionPrompt()
 
-} else if (data.employee_type == 'Engineer') {
-    const engineerData = await inquirer
-    .prompt([
-        {
-            name: 'name',
-            message: "What is the employee's full name?",
-            type: 'input',
-        },
-        {
-            name: 'id',
-            type: 'input',
-            message: "What is the employee's ID number?",
-        },
-        {
-            name: 'email',
-            type: 'input',
-            message: "What is the employee's email address?",
-        },
-        {
-            name: 'github',
-            message: "What is the employees's GitHub username?",
-            type: 'input',
-        },
-    ]
-    )
+    } else if (data.employee_type == 'Engineer') {
+        const engineerData = await inquirer
+            .prompt([
+                {
+                    name: 'name',
+                    message: "What is the employee's full name?",
+                    type: 'input',
+                },
+                {
+                    name: 'id',
+                    type: 'input',
+                    message: "What is the employee's ID number?",
+                },
+                {
+                    name: 'email',
+                    type: 'input',
+                    message: "What is the employee's email address?",
+                },
+                {
+                    name: 'github',
+                    message: "What is the employees's GitHub username?",
+                    type: 'input',
+                },
+            ]
+            )
 
-    const thisEngineer = new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.github)
-    
-    team.push(thisEngineer)
-    startProgram()
-    async function startProgram(){
-    let htmlDoc = render(team)
-    await fs.writeFile(outputPath, htmlDoc)
-}   
-    console.log(team)
-    //team.push(new Engineer(`${engineerData.name}, ${engineerData.id}, ${engineerData.email}, ${engineerData.github}`));
-    console.log(engineerData.github)
-} else{
-    const internData = await inquirer
-    .prompt([
-        {
-            name: 'name',
-            message: "What is the employee's full name?",
-            type: 'input',
-        },
-        {
-            name: 'id',
-            type: 'input',
-            message: "What is the employee's ID number?",
-        },
-        {
-            name: 'email',
-            type: 'input',
-            message: "What is the employee's email address?",
-        },
-        {
-            name: 'school',
-            message: "What is the intern's school name?",
-            type: 'input',
-        },
-    ]
-    )
-    const thisIntern = new Manager(internData.name, internData.id, internData.email, internData.school)
-    team.push(thisIntern)
-    startProgram()
-    async function startProgram(){
-    let htmlDoc = render(team)
-    await fs.writeFile(outputPath, htmlDoc)
-} 
-    console.log(internData.school)
-}
+        const thisEngineer = new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.github)
+
+        team.push(thisEngineer)
+        questionPrompt()
+        //team.push(new Engineer(`${engineerData.name}, ${engineerData.id}, ${engineerData.email}, ${engineerData.github}`));
+    } else if(data.employee_type == "Intern") {
+        const internData = await inquirer
+            .prompt([
+                {
+                    name: 'name',
+                    message: "What is the employee's full name?",
+                    type: 'input',
+                },
+                {
+                    name: 'id',
+                    type: 'input',
+                    message: "What is the employee's ID number?",
+                },
+                {
+                    name: 'email',
+                    type: 'input',
+                    message: "What is the employee's email address?",
+                },
+                {
+                    name: 'school',
+                    message: "What is the intern's school name?",
+                    type: 'input',
+                },
+            ]
+            )
+        const thisIntern = new Intern(internData.name, internData.id, internData.email, internData.school)
+        team.push(thisIntern)
+        questionPrompt()
+    }else {
+        renderHTML()
+    }
 }
 
-// addEmployee()
-// async function addEmployee (){
-// const addAnother  = await inquirer
-// .prompt([
 
-//     {
-//         name: 'add_member',
-//         type: 'list',
-//         message: "Would you like to add another team member?",
-//         choices: ["Yes", "No"],
-//     },
-
-// ])
-
-// if (addAnother.add_member == "Yes") {
-
-//     questionPrompt()
-
-// } else {
-
-//     console.log("All done! Open the team.html file to view you team.")
-
-// }
-
-// }
-
+function renderHTML() {
+    const template = render(team)
+    fs.writeFile("./output/team.html", template, (err) => {
+        if(err) throw err
+        else console.log("written a new file")
+    })
+}
